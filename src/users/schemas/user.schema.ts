263549f928c3c79702d4c4d09f113a5023bcdd01
LucaPetrescu <luca6011@gmail.com>
2024-05-23 09:00:43 +0300

    first commit

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  last_name: string;
+
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: true })
+  avatar: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
